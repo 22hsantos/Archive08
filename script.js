ea3a4fa21f01d6abaad99b1a15cd0a31fbf3bed7
22hsantos <22hsantos@wakatipu.school.nz>
2026-08-27 11:59:27 +1200

    feat: add gallery box fn

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //live clock
 function updateTime() {
+    if (!document.getElementById("hours")) return;//checks if "hours" exists, if not, return (stop)
+
     const now = new Date();//grabs date and stores it in now
     let hours = now.getHours();// grabs Hours from now and stores it in hours
     let minutes = now.getMinutes();
@@ -26,6 +28,8 @@ updateTime();
 
 //live weekday
 function updateDay(){
+    if (!document.getElementById('live-day')) return;//checks if "live-day" exists, if not, return (stop)
+
     const today = new Date();//grabs date and stores it in today
 
     const dayName = today.toLocaleString('en-NZ', {weekday:'long'})//converts plaint date to eng-NZ(locale) and weekday(option)
@@ -33,3 +37,28 @@ function updateDay(){
     document.getElementById('live-day').textContent = dayName;//updates live-day
 }
 updateDay();
+
+//image gallery
+const images = [
+    "assets/images/about_me/placeholder_cat.jpeg",//0
+    "assets/images/general/placeholder_img.gif"//1
+];
+
+let currentIndex = 0;// "let" defines a value that can be changed.
+
+function updateImage() {//updates the image in the gallery
+    const imgElement = document.getElementById("gallery-img");//grabs gallery img into imgElement
+    imgElement.src = images[currentIndex];//updates src of Gallery img to currentIndex
+}
+
+function nextImage(){
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+}
+
+function prevImage(){
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage()
+}
+
+updateImage();
